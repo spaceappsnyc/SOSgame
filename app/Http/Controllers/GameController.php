add3fed50bf828b0fd30d1fade0eda18d4367b4a
Hamza EL GARRAB <elgarrabhamza@gmail.com>
2018-10-21 08:45:52 +0200

    Items Webservice Method added

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -14,7 +14,7 @@ class GameController extends Controller
       public function start(Request $request)
       {
 
-            $disaster = Disaster::find(rand(1,Disaster::count()));
+            // ADD CONDITION OF A MINIMUM OF 1 ADULT / SENIOR
 
             $characters = Character::get()->all();
             shuffle($characters);
@@ -32,6 +32,7 @@ class GameController extends Controller
             $game = new stdClass();
             $game->id = Uuid::generate()->string;
 
+            $disaster = Disaster::find(rand(1,Disaster::count()));
             $game->disaster = $disaster;
             $game->disaster->generated_level = rand(1,3);
 
@@ -40,4 +41,5 @@ class GameController extends Controller
             return json_decode(json_encode($game), true);
 
       }
+
 }
